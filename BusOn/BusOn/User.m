272d3dfae831db356d4c699602c018fca25ddda7
Helmiton Moraes da Silva Cunha Junior <hmscj@imac01.local>
2014-03-22 16:03:54 -0300

    Teste criando classe usuario. Btw, objective c sucks.

---
+++ BusOn/BusOn/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  BusOn
+//
+//  Created by Helmiton Moraes da Silva Cunha Junior on 22/03/14.
+//  Copyright (c) 2014 Pedro Augusto Silva Lucena. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
