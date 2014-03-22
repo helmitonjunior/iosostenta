2640aed1f32bd9771a3ea18ebd3712bc3462d1ec
Pedro Augusto Silva Lucena <pasl@imac02.local>
2014-03-22 17:24:16 -0300

    p

--- BusOn/BusOn/ViewController.m
+++ BusOn/BusOn/ViewController.m
@@ -9,11 +9,25 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *emailText;
+@property (weak, nonatomic) IBOutlet UITextField *passText;
+//@property (strong, nonatomic) NSString *email;
+//@property (strong, nonatomic) NSString *password;
 
 @end
 
 @implementation ViewController
 
+- (IBAction)LoginClick:(id)sender {
+    NSString *email = _emailText.text;
+    NSString *password = _passText.text;
+    if (email){
+        
+    }
+    
+        
+
+    }
 
 
 @end
