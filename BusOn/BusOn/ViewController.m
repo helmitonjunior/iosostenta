542d5bde1f20d1fce23b62d9b8bae979d1bb2b93
Helmiton Moraes da Silva Cunha Junior <hmscj@imac01.local>
2014-03-26 13:44:15 -0300

    Att

--- BusOn/BusOn/ViewController.m
+++ BusOn/BusOn/ViewController.m
@@ -7,14 +7,12 @@
 //
 
 #import "ViewController.h"
-#import "Login.h"
+
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *emailText;
 @property (weak, nonatomic) IBOutlet UITextField *passText;
-@property (strong, nonatomic) Login *login;
-//@property (strong, nonatomic) NSString *email;
-//@property (strong, nonatomic) NSString *password;
+
 
 @end
 
@@ -34,10 +32,79 @@
     	}else if(!validPassword){
     		[self alertStatus:@"Sua senha deve ter no mínimo 6 caracteres" :@"Login falhou" :0];
     	}else{
-    		[self.login LoginWithEmail:email andPassword:password andSender:sender];
+    		[self LoginWithEmail:email andPassword:password andSender:sender];
     	}
         
         
+    }
+}
+
+- (void) LoginWithEmail:(NSString *) email andPassword:(NSString *) password andSender:(id) sender
+{
+	NSInteger success = 0;
+    @try {
+        
+        
+        NSString *post =[[NSString alloc] initWithFormat:@"username=%@&password=%@", email, password];
+        NSLog(@"PostData: %@",post);
+        
+        NSURL *url=[NSURL URLWithString:@"https://buson.info/php/"];
+        
+        NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+        
+        NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
+        
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+        [request setURL:url];
+        [request setHTTPMethod:@"POST"];
+        [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+        [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+        [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+        [request setHTTPBody:postData];
+        
+        //[NSURLRequest setAllowsAnyHTTPSCertificate:YES forHost:[url host]];
+        
+        NSError *error = [[NSError alloc] init];
+        NSHTTPURLResponse *response = nil;
+        NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+        
+        NSLog(@"Response code: %ld", (long)[response statusCode]);
+        
+        if ([response statusCode] >= 200 && [response statusCode] < 300)
+        {
+            NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
+            NSLog(@"Response ==> %@", responseData);
+            
+            NSError *error = nil;
+            NSDictionary *jsonData = [NSJSONSerialization
+                                      JSONObjectWithData:urlData
+                                      options:NSJSONReadingMutableContainers
+                                      error:&error];
+            
+            success = [jsonData[@"success"] integerValue];
+            NSLog(@"Success: %ld",(long)success);
+            
+            if(success == 1)
+            {
+                NSLog(@"Login SUCCESS");
+            } else {
+                
+                NSString *error_msg = (NSString *) jsonData[@"error_message"];
+                [self alertStatus:error_msg :@"Sign in Failed!" :0];
+            }
+            
+        } else {
+            //if (error) NSLog(@"Error: %@", error);
+            [self alertStatus:@"Connection Failed" :@"Sign in Failed!" :0];
+        }
+        
+    }
+    @catch (NSException * e) {
+        NSLog(@"Exception: %@", e);
+        [self alertStatus:@"Sign in Failed." :@"Error!" :0];
+    }
+    if (success) {
+        [self performSegueWithIdentifier:@"login_success" sender:self];
     }
 }
 
