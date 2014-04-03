1dc194d9f56234497bca0f9fbb9f54132ab4df34
Pedro Augusto Silva Lucena <pasl@imac01.local>
2014-04-03 10:52:46 -0300

    comeco do cadastro

---
+++ BusOn/BusOn/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  BusOn
+//
+//  Created by Pedro Augusto Silva Lucena on 03/04/14.
+//  Copyright (c) 2014 Pedro Augusto Silva Lucena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
